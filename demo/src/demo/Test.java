5da056ce0d3c0d8159af4aa151d92a140a4b6065
sdac-24 <sdac-24@sdac-24-PC>
2024-02-02 17:33:14 +0530

    tokens

--- demo/src/demo/Test.java
+++ demo/src/demo/Test.java
@@ -4,8 +4,7 @@ public class Test {
   public static void main(String[] args) {
 	System.out.println("Yoo!! People");
 	System.out.println("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");
-	System.out.println("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");
-
+	System.out.println("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");
 	
 }
 }
