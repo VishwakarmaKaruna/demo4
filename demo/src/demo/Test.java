a591364c1c6d002f7bcf6ec7de271685058404b3
sdac-24 <sdac-24@sdac-24-PC>
2024-02-02 17:08:53 +0530

    3.0

--- demo/src/demo/Test.java
+++ demo/src/demo/Test.java
@@ -5,5 +5,7 @@ public class Test {
 	System.out.println("Yoo!! People");
 	System.out.println("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");
 	System.out.println("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");
+
+	System.out.println("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");
 }
 }
