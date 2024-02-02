4e1ae1e7ec42080319d639afc8a555129ff77958
sdac-24 <sdac-24@sdac-24-PC>
2024-02-02 17:03:06 +0530

    update 2

--- demo/src/demo/Test.java
+++ demo/src/demo/Test.java
@@ -4,5 +4,6 @@ public class Test {
   public static void main(String[] args) {
 	System.out.println("Yoo!! People");
 	System.out.println("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");
+	System.out.println("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");
 }
 }
